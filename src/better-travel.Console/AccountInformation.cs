eb3c476197512119ce87e6587d36bb89ea2ceb0f
Daniall <angrydaniall123456@gmail.com>
2020-05-12 01:39:32 +0300

    Added authorization in instagram

---
+++ src/better-travel.Console/AccountInformation.cs
@@ -0,0 +1,30 @@
+﻿namespace better_travel.Console
+{
+    public class AccountInformation
+    {
+        public string InstagramUesrname
+        {
+            get { return instagramUesrname; }
+        }
+
+        public string InstagramPassword
+        {
+            get { return instagramPassword; }
+        }
+
+        private string instagramUesrname = "yapaw85310";
+        private string instagramPassword = "yapaw85310@box4mls.com";
+    }
+
+    public class Instagram
+    {
+        public string uesrname = "yapaw85310";
+        public string pasword = "yapaw85310@box4mls.com";
+    }
+
+    public class Facebook
+    {
+        public string Date { get; }
+        public string Text { get; }
+    }
+}
