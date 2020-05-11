e55480f16085d826d6a4326a22a8e516dc887f39
Daniall <angrydaniall123456@gmail.com>
2020-05-11 23:57:39 +0300

    Added information scratching for Instagram posts

---
+++ src/better-travel.Console/Descritption.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using HtmlAgilityPack;
+
+namespace better_travel.Console
+{
+    public class Descritption
+    {
+        public string Date { get; set; }
+        public string Text { get; set; }
+        public IEnumerable<HtmlNode> Hashtag { get; set; }
+    }
+}
