1e19578d57e8d5e07c500718c8d0e956af14d666
Daniall <angrydaniall123456@gmail.com>
2020-05-17 23:10:53 +0300

    Added GetPost and VisitHashtagUrl methods

---
+++ src/better-travel.BL/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace better_travel.BL
+{
+    public class Class1
+    {
+    }
+}
--- src/better-travel.Console/AccountInformation.cs
+++ src/better-travel.Console/AccountInformation.cs
@@ -1,30 +1,34 @@
-﻿namespace better_travel.Console
+﻿using System;
+
+namespace better_travel.Console
 {
     public class AccountInformation
     {
-        public string InstagramUesrname
-        {
-            get { return instagramUesrname; }
-        }
+        public string InstagramUesrname => _instagramUesrname;
 
-        public string InstagramPassword
-        {
-            get { return instagramPassword; }
-        }
+        public string InstagramPassword => _instagramPassword;
 
-        private string instagramUesrname = "yapaw85310";
-        private string instagramPassword = "yapaw85310@box4mls.com";
-    }
+        public string BaseUrl => "https://www.instagram.com";
 
-    public class Instagram
-    {
-        public string uesrname = "yapaw85310";
-        public string pasword = "yapaw85310@box4mls.com";
-    }
+        public string UserNameSelector =>
+            "#react-root > section > main > div > article > div > div:nth-child(1) > div > form > div:nth-child(2) > div > label > input";
 
-    public class Facebook
-    {
-        public string Date { get; }
-        public string Text { get; }
+        public string PasswordSelector =>
+            "#react-root > section > main > div > article > div > div:nth-child(1) > div > form > div:nth-child(3) > div > label > input";
+
+        public string SubmitBtnSelector =>
+            "#react-root > section > main > div > article > div > div:nth-child(1) > div > form > div:nth-child(4) > button";
+
+        public string PostСloseButton => "div.Igw0E.IwRSH.eGOV_._4EzTm.BI4qX.qJPeX.fm1AK.TxciK.yiMZG > button > svg";
+
+        public string HashTagsBaseClass => ".EZdmt";
+
+        public string[] Hashtag => new[] {"Geeks", "GFG", "travel", "dota", "apex"};
+
+        private readonly string _instagramUesrname = "disayo8154";
+
+        private readonly string _instagramPassword = "disayo8154@inbov03.com";
+        /*      private readonly string _instagramUesrname = "yapaw85310";
+        private readonly string _instagramPassword = "yapaw85310@box4mls.com";*/
     }
 }
