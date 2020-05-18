b4d329916de56a68b00bbadb2e07e60c2e8eaad1
Daniall <angrydaniall123456@gmail.com>
2020-05-19 01:42:28 +0300

    .gitignore is now working

---
+++ src/better-travel.Console/PostInfo.cs
@@ -0,0 +1,17 @@
+﻿namespace better_travel.Console
+{
+    public class PostInfo
+    {
+        public Descritption Description { get; set; }
+        public string ImgUrl { get; set; }
+        public string Author { get; set; }
+
+
+        public PostInfo(Descritption description, string imgUrl, string author)
+        {
+            ImgUrl = imgUrl;
+            Description = description;
+            Author = author;
+        }
+    }
+}
