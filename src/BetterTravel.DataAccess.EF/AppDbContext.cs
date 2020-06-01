999a7d63cbade13358d74dcf6924bf699af98072
Alex 'iTKerry' Katrinets <a.katrynets@gmail.com>
2020-06-01 17:34:59 +0300

    stage #6

--- src/BetterTravel.DataAccess.EF/AppDbContext.cs
+++ src/BetterTravel.DataAccess.EF/AppDbContext.cs
@@ -6,5 +6,11 @@ namespace BetterTravel.DataAccess.EF
     {
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) => 
             Database.EnsureCreated();
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(AppDbContext).Assembly);
+        }
     }
 }
---
+++ src/BetterTravel.DataAccess.EF/Migrations/20200601142857_InitialCreate.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BetterTravel.DataAccess.EF.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ToursInfo",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    PostUrl = table.Column<string>(nullable: true),
+                    ImgUrl = table.Column<string>(nullable: true),
+                    Author = table.Column<string>(nullable: true),
+                    Date = table.Column<string>(nullable: true),
+                    Text = table.Column<string>(nullable: true),
+                    HashTags = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ToursInfo", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ToursInfo");
+        }
+    }
+}
